da6110ff4dc51ce438af12ce7bfd493e6a6bfaed
Largoet <prunet.t@gmail.com>
2024-10-22 11:56:46 +0200

    Creation carte pokemon

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://www.pngplay.com/wp-content/uploads/11/Gengar-PNG-Background.png"
+        alt="GENGAR"
+      />
+      <figcaption>GENGAR</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
