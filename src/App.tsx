bfe9916e4cffbdc4dd8d430f8b9ad759809b1561
Largoet <prunet.t@gmail.com>
2024-10-23 11:55:59 +0200

    fucking props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import PokemonCard from "./components/pokemoncard";
 
+const pokemonList = [
+  {
+    name: "gengar",
+
+    imgSrc:
+      "https://www.pngplay.com/wp-content/uploads/11/Gengar-PNG-Background.png",
+  },
+
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,18 +1,4 @@
-const pokemonList = [
-  {
-    name: "gengar",
-
-    imgSrc:
-      "https://www.pngplay.com/wp-content/uploads/11/Gengar-PNG-Background.png",
-  },
-
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
